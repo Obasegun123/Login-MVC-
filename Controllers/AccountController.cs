878dda705e20c82d544f2e554f99b0138516c5ac
obasooto segun <52790560+Obasegun123@users.noreply.github.com>
2021-11-02 11:29:26 +0100

    Add project files.

---
+++ Controllers/AccountController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Login_MVC_.Models;
+
+namespace Login_MVC_.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Verify(Account acc) 
+        {
+            return View();
+        }
+    }
+}
---
+++ Models/Account.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Login_MVC_.Models
+{
+    public class Account
+    {
+        public string Name { get; set; }
+        [Required(ErrorMessage  = "please enter a valid user name")]
+        public string Password { get; set; }
+    }
+}
